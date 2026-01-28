679d64288463c23428b6085b13a56567e158adeb
Deák Ákos 205 <deaako205@logiker.hu>
2026-01-28 14:07:08 +0100

    OrdersTable

--- sql.sql
+++ sql.sql
@@ -1,1 +1,9 @@
-CREATE DATABASE KreativAdatBazisNev;
+CREATE TABLE Orders (
+    OrderID int NOT NULL AUTO_INCREMENT,
+    OrderDate DateTime,
+    UserID int,
+    ProductID int,
+    PRIMARY KEY(OrderID),
+    FOREIGN KEY (UserID) REFERENCES users(UserID),
+    FOREIGN KEY (ProductID) REFERENCES products(ProductID)
+    );
