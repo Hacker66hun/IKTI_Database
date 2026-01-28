d2c7ce10ae14f93991481e7557f063473caf9bdb
Suhajda Bálint 238 <suhbal238@logiker.hu>
2026-01-28 14:05:22 +0100

    modified users table

--- sql.sql
+++ sql.sql
@@ -3,15 +3,14 @@ CREATE DATABASE Webshop
 
 
 CREATE TABLE Users(
-    UserId INT,
+    UserId INT(3),
     customer_name VARCHAR(40),
     customer_tel VARCHAR(20),
     customer_email VARCHAR(30),
-    address_id INT,
+    address_id INT(30),
 
-    PRIMARY KEY(UserId),
-    INDEX(customer_id),
-    FOREIGN KEY(customer_id) 
+    PRIMARY KEY(UserId)
+    FOREIGN KEY(customer_id) references Orders(/*"másik tábla primary"*/)
 );
 
 
