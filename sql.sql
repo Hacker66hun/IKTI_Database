243f50ceb3d6a370f637702bdb2382f72e10f926
AkosDeak <134687268+AkosDeak@users.noreply.github.com>
2026-02-05 17:34:13 +0100

    Add files via upload

--- sql.sql
+++ sql.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Products (
     ProductID int NOT NULL AUTO_INCREMENT,
-    ProductName varchar
-    Price decimal
+    ProductName varchar,
+    Price decimal,
     Stock int,
     PRIMARY KEY(ProductID)
 );
